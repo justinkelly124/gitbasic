9b683ec372c7e84d22eb486f2e2cb47024dcaba3
Justin Kelly <justink124@outlook.com>
2020-03-31 14:37:22 -0400

    added functionality to hello.java

--- hello.java
+++ hello.java
@@ -1,4 +1,12 @@
 public class hello{
+	
+	public void doSomething(){
+		//doing something
+		int x = 1;
+		int y = 2;
+		int z = x + y;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 	}
